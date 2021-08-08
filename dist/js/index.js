213f67d1d1890db58aef64c820689aca3ab356cb
Davin McDonald <davin@solidmedium.com.au>
2021-08-08 13:05:34 +1000

    establish data JSON, add initial render functions for todo list and btns

--- dist/js/index.js
+++ dist/js/index.js
@@ -1,13 +1,44 @@
 'use strict';
 
-function component() {
-  var element = document.getElementById('app-mount-point');
-  var table = "\n    <table class=\"table table-bordered\">\n      <thead>\n        <tr>\n          <th>Todo</th>\n          <th>Actions</th>\n        </tr>\n      </thead>\n      <tbody>\n        <tr>\n          <td></td>\n        </tr>\n      </tbody>\n    </table>\n  "; // Lodash, now imported by this script
-  // element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+var data = [{
+  id: 1,
+  name: 'Take out the trash',
+  priority: 0,
+  complete: 'No',
+  publish: true
+}, {
+  id: 2,
+  name: 'Bring in the trash',
+  priority: 0,
+  complete: 'No',
+  publish: true
+}, {
+  id: 3,
+  name: 'Walk the dog',
+  priority: 0,
+  complete: 'No',
+  publish: false
+}];
 
-  element.innerHTML = table;
-  return element;
+function renderTable() {
+  var dataIsolated = data.filter(function (p) {
+    return p.publish;
+  });
+  var rows = [];
+  dataIsolated.map(function (items) {
+    var btns = "\n      <button type=\"button\" class=\"btn btn-delete\">Delete</button>\n      <button type=\"button\" class=\"btn btn-complete\">Complete</button>\n    ";
+    rows += "\n        <tr>\n          <td>".concat(items.name, "</td>\n          <td>").concat(items.priority, "</td>\n          <td>").concat(items.complete, "</td>\n          <td>").concat(btns, "</td>\n        </tr>\n    ");
+  });
+  var addBtn = "\n    <div class=\"text-right\">\n      <button type=\"button\" class=\"btn btn-launch-modal\">Add New Todo +</button>\n    </div>\n  ";
+  var table = "\n    ".concat(addBtn, "\n    <table class=\"table table-bordered\">\n      <thead>\n        <tr>\n          <th>Todo</th>\n          <th>Priority</th>\n          <th>Complete</th>\n          <th>Actions</th>\n        </tr>\n      </thead>\n      <tbody>\n        ").concat(rows, "\n      </tbody>\n    </table>\n  ");
+  return table;
 }
 
-document.body.appendChild(component());
+function loadApp() {
+  var tableHTML = renderTable();
+  var table = document.getElementById('app-mount-point');
+  table.innerHTML = tableHTML;
+}
+
+document.addEventListener("DOMContentLoaded", loadApp);
 //# sourceMappingURL=index.js.map
--- src/js/index.js
+++ src/js/index.js
@@ -1,29 +1,81 @@
-function component() {
-  const element = document.getElementById('app-mount-point');
+let data = [
+  {
+    id: 1,
+    name: 'Take out the trash',
+    priority: 0,
+    complete: 'No',
+    publish: true
+  }, {
+    id: 2,
+    name: 'Bring in the trash',
+    priority: 0,
+    complete: 'No',
+    publish: true
+  },  {
+    id: 3,
+    name: 'Walk the dog',
+    priority: 0,
+    complete: 'No',
+    publish: false
+  } 
+];
+
+function renderTable() {
+
+  const dataIsolated = data.filter(p => p.publish);
+
+  let rows = [];
+  dataIsolated.map(items => {
+    
+    const btns = `
+      <button type="button" class="btn btn-delete">Delete</button>
+      <button type="button" class="btn btn-complete">Complete</button>
+    `;
+
+    rows += `
+        <tr>
+          <td>${items.name}</td>
+          <td>${items.priority}</td>
+          <td>${items.complete}</td>
+          <td>${btns}</td>
+        </tr>
+    `;
+  });
+
+  const addBtn = `
+    <div class="text-right">
+      <button type="button" class="btn btn-launch-modal">Add New Todo +</button>
+    </div>
+  `;
 
   const table = `
+    ${addBtn}
     <table class="table table-bordered">
       <thead>
         <tr>
           <th>Todo</th>
+          <th>Priority</th>
+          <th>Complete</th>
           <th>Actions</th>
         </tr>
       </thead>
       <tbody>
-        <tr>
-          <td></td>
-        </tr>
+        ${rows}
       </tbody>
     </table>
   `;
 
-  // Lodash, now imported by this script
-  // element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-  element.innerHTML = table;
-
-
-
-  return element;
+  return table;
 }
 
-document.body.appendChild(component());
+function loadApp() {
+  
+  const tableHTML = renderTable();
+  const table = document.getElementById('app-mount-point');
+
+  table.innerHTML = tableHTML;
+
+}
+
+document.addEventListener("DOMContentLoaded", loadApp);
+
